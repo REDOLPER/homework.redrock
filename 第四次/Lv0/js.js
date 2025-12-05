77a0eb12b955b00e5684800d5e46de081eb6af50
REDOLPER <REDOLPER_lp@outlook.com>
2025-12-05 20:49:51 +0800

    只是作业（希望提交成功[heart]）

---
+++ 第四次/Lv0/js.js
@@ -0,0 +1,19 @@
+let obj1 = new Object();
+obj1.name = 'Kris';
+obj1.age = 'How can I know';
+
+let obj2 = {
+  name: 'Susie',
+  age: 'I don\'t want my face to be eaten',
+}
+
+function Person(){
+  this.name = 'Ralsei';
+  this.age = 'Apparently he can live forever?';
+}
+
+let obj3 = new Person();
+
+console.log (obj1);
+console.log (obj2);
+console.log (obj3);
